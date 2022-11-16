19bb671528c446b8a52119482373f965ded90d35
damanectofficial <88991901+damanectofficial@users.noreply.github.com>
2022-11-17 00:54:18 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,22 +27,22 @@ global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di 
 global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Hidayat Store' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Hidayat-Store.My.Id' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Digital Marketing Agency' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = '#'
+global.ig = '#'
+global.mygc = '#'
+global.myweb = 'https://hidayat-store.my.id'
+global.email = '#'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Hidayat-Store.My.Id' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Digital Marketing Agency' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
